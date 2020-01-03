44ec5e7c473d8676157483b8d2063bb7a0dc6d8a
ubett <ubetapatis90@gmail.com>
2020-01-03 08:47:05 +0700

    Updates

---
+++ precache-manifest.9130d975b0d79df001f258a86576c790.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567bce98c778d59fa9613c17de012082",
+    "url": "/alienrobo/index.html"
+  },
+  {
+    "revision": "c226e32e6f8431b389a0",
+    "url": "/alienrobo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8d3cf65515b99af7be8f",
+    "url": "/alienrobo/static/css/main.89a1b374.chunk.css"
+  },
+  {
+    "revision": "c226e32e6f8431b389a0",
+    "url": "/alienrobo/static/js/2.b9fefc33.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/alienrobo/static/js/2.b9fefc33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d3cf65515b99af7be8f",
+    "url": "/alienrobo/static/js/main.a607f9e2.chunk.js"
+  },
+  {
+    "revision": "3856b7ad787c5d2be06d",
+    "url": "/alienrobo/static/js/runtime-main.3617cd88.js"
+  }
+]);
